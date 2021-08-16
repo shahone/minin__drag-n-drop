24af866bf710891a3c5ff2c5fb3336d03c94923c
Ruslan <62812180+shahone@users.noreply.github.com>
2021-08-16 15:41:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+window.onload = function () {
+  const item = document.querySelector('.item');
+  const placeholders = document.querySelectorAll('.placeholder');
+
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
+
+  for (let placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragOver);
+    placeholder.addEventListener('dragenter', dragEnter);
+    placeholder.addEventListener('dragleave', dragLeave);
+    placeholder.addEventListener('drop', dragDrop);
+  }
+
+  function dragStart(e) {
+    e.target.classList.add('hold');
+
+    setTimeout(() => {
+      e.target.classList.add('hide');
+    }, 0);
+  }
+
+  function dragEnd(e) {
+    e.target.classList.remove('hold', 'hide');
+    // e.target.className = 'item';
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  function dragEnter(e) {
+    e.target.classList.add('hovered');
+  }
+
+  function dragLeave(e) {
+    e.target.classList.remove('hovered');
+  }
+
+  function dragDrop(e) {
+    e.target.append(item);
+    e.target.classList.remove('hovered');
+  }
+
+
+};
